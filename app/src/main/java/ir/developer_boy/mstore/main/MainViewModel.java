bf79d6d18650d978c8b940f030ad6b0bf23f3f8c
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-09 02:52:00 +0430

    add new codes

--- app/src/main/java/ir/developer_boy/mstore/main/MainViewModel.java
+++ app/src/main/java/ir/developer_boy/mstore/main/MainViewModel.java
@@ -1,9 +1,17 @@
 package ir.developer_boy.mstore.main;
 
+import java.util.List;
+
+import io.reactivex.Single;
+import ir.developer_boy.mstore.model.Product;
 import ir.developer_boy.mstore.model.api.ApiService;
 import ir.developer_boy.mstore.model.providers.ApiServiceProvider;
 
 public class MainViewModel {
     private ApiService apiService= ApiServiceProvider.getApiService();
 
+
+    public Single<List<Product>> latestProducts(){
+        return apiService.getProducts(0);
+    }
 }
