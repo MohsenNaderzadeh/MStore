3597e887f6dc4287d3e50acc442842762fbed057
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-09 02:47:30 +0430

    add new codes

--- app/src/main/java/ir/developer_boy/mstore/main/MainActivity.java
+++ app/src/main/java/ir/developer_boy/mstore/main/MainActivity.java
@@ -1,7 +1,9 @@
-package ir.developer_boy.mstore;
+package ir.developer_boy.mstore.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ir.developer_boy.mstore.R;
 
 public class MainActivity extends AppCompatActivity {
 
