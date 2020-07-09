06b07353a3d643b4365dc0399ebe756c65e670ec
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-09 14:41:16 +0430

    Add Some methods for getting different sort of products like popular,latest and... .

--- app/src/main/java/ir/developer_boy/mstore/main/MainViewModel.java
+++ app/src/main/java/ir/developer_boy/mstore/main/MainViewModel.java
@@ -3,6 +3,7 @@ package ir.developer_boy.mstore.main;
 import java.util.List;
 
 import io.reactivex.Single;
+import ir.developer_boy.mstore.model.Banner;
 import ir.developer_boy.mstore.model.Product;
 import ir.developer_boy.mstore.model.api.ApiService;
 import ir.developer_boy.mstore.model.providers.ApiServiceProvider;
@@ -12,6 +13,20 @@ public class MainViewModel {
 
 
     public Single<List<Product>> latestProducts(){
-        return apiService.getProducts(0);
+        return apiService.getProducts(Product.SORT_LATEST);
     }
+
+    public Single<List<Product>> popularProducts(){
+        return  apiService.getProducts(Product.SORT_POPULAR);
+    }
+    public Single<List<Product>> productsPriceHighToLow(){
+        return  apiService.getProducts(Product.SORT_PRICE_HIGH_TO_LOW);
+    }
+    public Single<List<Product>> productsPriceLowToHigh(){
+        return  apiService.getProducts(Product.SORT_PRICE_LOW_TO_HIGH);
+    }
+    public Single<List<Banner>> banners(){
+        return  apiService.getBanners();
+    }
+
 }
