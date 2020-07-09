22662a4965ccfed9b373ca108a65ef37a7650fbb
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-09 14:43:24 +0430

    A  custom class for handling Single Observer Repeative methods like OnSubscribe and OnError .

---
+++ app/src/main/java/ir/developer_boy/mstore/model/api/MsSingleObserver.java
@@ -0,0 +1,28 @@
+package ir.developer_boy.mstore.model.api;
+
+import org.greenrobot.eventbus.EventBus;
+
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import ir.developer_boy.mstore.base.BaseActivity;
+import ir.developer_boy.mstore.exception.ExceptionMessageFactory;
+
+public abstract class MsSingleObserver<T> implements SingleObserver<T> {
+
+    private CompositeDisposable compositeDisposable;
+
+    public MsSingleObserver(CompositeDisposable compositeDisposable) {
+        this.compositeDisposable = compositeDisposable;
+    }
+
+    @Override
+    public void onSubscribe(Disposable d) {
+        compositeDisposable.add(d);
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        EventBus.getDefault().post(ExceptionMessageFactory.getMessage(e));
+    }
+}
