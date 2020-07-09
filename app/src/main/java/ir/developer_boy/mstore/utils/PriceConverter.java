243d85663650578f6ba107ffec409ee6cda7b085
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-09 15:15:30 +0430

    Add Price Converter class for contract currency with product price

---
+++ app/src/main/java/ir/developer_boy/mstore/utils/PriceConverter.java
@@ -0,0 +1,9 @@
+package ir.developer_boy.mstore.utils;
+
+public class PriceConverter {
+
+    public static String convert(long price)
+    {
+        return String.valueOf(price)+"تومان";
+    }
+}
