6ad355ac958e7cc12f0a6c8f131a2ac57fa2c83c
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-09 14:45:28 +0430

    Add Adapter

---
+++ app/src/main/java/ir/developer_boy/mstore/main/ProductAdapter.java
@@ -0,0 +1,63 @@
+package ir.developer_boy.mstore.main;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ir.developer_boy.mstore.R;
+import ir.developer_boy.mstore.model.Product;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private List<Product> productList=new ArrayList<>();
+
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ProductViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        holder.bindProduct(productList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public class ProductViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView productImage;
+        private TextView productTitle;
+        private TextView productPrevPrice;
+        private TextView productPrice;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+            productImage=itemView.findViewById(R.id.iv_product_Image);
+            productTitle=itemView.findViewById(R.id.tv_product_title);
+            productPrevPrice=itemView.findViewById(R.id.tv_product_prev_price);
+            productPrice=itemView.findViewById(R.id.tv_product_price);
+        }
+
+        public void bindProduct(Product product)
+        {
+            Picasso.get().load(product.getImage()).into(productImage);
+            productTitle.setText(product.getTitle());
+            productPrevPrice.setText(String.valueOf(product.getPreviousPrice()));
+            productPrice.setText(String.valueOf(product.getPrice()));
+        }
+    }
+}
