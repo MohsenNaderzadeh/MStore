7d5174f677540f441ffe839a24aaf801992c7076
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-10 14:40:19 +0430

    add some spaces.

--- app/src/main/java/ir/developer_boy/mstore/utils/PriceConverter.java
+++ app/src/main/java/ir/developer_boy/mstore/utils/PriceConverter.java
@@ -4,6 +4,6 @@ public class PriceConverter {
 
     public static String convert(long price)
     {
-        return String.valueOf(price)+"تومان";
+        return String.valueOf(price)+" تومان";
     }
 }
