8cb8c3a8266196a6712ddf366c939de943501b73
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-13 17:37:29 +0430

    add new method for handle Unauthorized exception from eventbus handler

--- app/src/main/java/ir/developer_boy/mstore/base/BaseActivity.java
+++ app/src/main/java/ir/developer_boy/mstore/base/BaseActivity.java
@@ -1,5 +1,6 @@
 package ir.developer_boy.mstore.base;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -10,9 +11,12 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import io.reactivex.disposables.CompositeDisposable;
+import ir.developer_boy.mstore.auth.AuthenticationActivity;
+import ir.developer_boy.mstore.exception.UnAthuorized;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+    private static final String TAG = "BaseActivity";
     public abstract int getCoordinatetorLayoutId();
     protected CompositeDisposable compositeDisposable=new CompositeDisposable();
 
@@ -21,17 +25,31 @@ public abstract class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EventBus.getDefault().register(this);
     }
+
+
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void ShowSnackBarMessage(String message){
 
         Snackbar.make(findViewById(getCoordinatetorLayoutId()), message, Snackbar.LENGTH_LONG).show();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EventBus.getDefault().unregister(this);
+        compositeDisposable.clear();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void handleUnAuthorizedException(UnAthuorized unAthuorized) {
+        if (!(this instanceof AuthenticationActivity)) {
+            startActivity(new Intent(this, AuthenticationActivity.class));
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        EventBus.getDefault().unregister(this);
-        compositeDisposable.clear();
+
     }
 }
