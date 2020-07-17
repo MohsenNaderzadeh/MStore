d3330b79d45f79671dd8c7f73d30f27f801fe4b4
MohsenNaderzadeh <naderzade.mohsen@yahoo.com>
2020-07-17 14:53:37 +0430

    checkout activity java file

---
+++ app/src/main/java/ir/developer_boy/mstore/checkout/CheckOutActvity.java
@@ -0,0 +1,15 @@
+package ir.developer_boy.mstore.checkout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ir.developer_boy.mstore.R;
+
+public class CheckOutActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out_actvity);
+    }
+}
